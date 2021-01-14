bdcf925f404672f9aeeee0030cc2c97dce3ec21f
Marina Krivcun <marinaktsn@gmail.com>
2021-01-14 10:43:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import asyncio
-
+from collections import deque
+from datetime import datetime
+import json
 import tornado.ioloop
 import tornado.locks
 import tornado.web
@@ -24,24 +26,23 @@ class MessagesBuffer():
 
     def __init__(self):
         self.cond = tornado.locks.Condition()
-        self.__messages = []
-        self.__messages_size = 500        
+        self.__messages = deque(maxlen=100)
+        # self.__messages = []
+        # self.__messages_size = 500        
 
 
     def add_message(self, message):
         self.__messages.append(message)
-
-        if len(self.__messages) > self.__messages_size:
-            self.__messages = self.messages[-self.__messages_size]
+        # print(self.__messages)
+        # if len(self.__messages) > self.__messages_size:
+        #     self.__messages = self.messages[-self.__messages_size]
         self.cond.notify_all()
-
 
     def get_messages(self):
         return self.__messages
 
     def get_messages_since(self, cursor):
         results = []
-
         for msg in reversed(self.__messages):
             if msg['id'] == cursor:
                 break
@@ -56,54 +57,60 @@ globalmessagebuffer = MessagesBuffer()
 class IndexHandler(tornado.web.RequestHandler):
     # отображать какую-то индексную страницу
     def get(self):
-        self.render("index.html", messages = globalmessagebuffer.get_messages())
+        self.render("index.html", messages = {})
 
+
+class StatHandler(tornado.web.RequestHandler):
+    def get(self):
+        stat = {}
+        messages = globalmessagebuffer.get_messages()
+        stat['all'] = len(messages)
+        for m in messages:
+            if m['user_id'] not in stat:
+                stat[m['user_id']] = 1
+            else:
+                stat[m['user_id']] += 1
+        # print(stat)
+        self.render("stat.html", statistics = stat)
 
 
 class EchoWebSocketHandler(tornado.websocket.WebSocketHandler):
-    
-    def open(self):
-        print('websocket is opened')
 
+    def open(self):
+        import uuid
+        self.current_user = uuid.uuid4()
+        print('websocket is opened')
 
     def on_message(self, message):
         import uuid
         id = uuid.uuid4()
-        new_message = {'id': str(id),  'message': message}
-        
+        # print(str(self.current_user))
+        message = json.loads(message)
+        new_message = {'id': str(id),  'user_id': str(self.current_user), 'to_user_id': message['to_user_id'], 'nickname': message['nickname'], 'message': message['msgtxt']}
         globalmessagebuffer.add_message(new_message)
-
-        print(f"Message #{ new_message.get('id', 'Unknown ID') } {new_message.get('message', 'Empty message')}")
+        print(f"Message #{ new_message.get('id', 'Unknown ID') } {new_message.get('user_id', 'Unknown ID')} {new_message.get('message', 'Empty message')}")
         self.write_message(f"{new_message}")
-
+        
     def on_close(self):
         print('websocket is closed')
 
 
 class MessageUpdatesHandler(tornado.web.RequestHandler):
     """docstring for MessageUpdatesHandler"""
-
     async def post(self):
-
         cursor = self.get_argument("cursor", None)
-
-        
         messages = globalmessagebuffer.get_messages_since(cursor)
-
         while not messages:
             self.wait_future = globalmessagebuffer.cond.wait()
             try:
                 await self.wait_future
             except asyncio.CancelledError:
                 return 
-
             messages = globalmessagebuffer.get_messages_since(cursor)
-
         if self.request.connection.stream.closed():
             return
-
-        print(f"cursor {cursor} ")
-        print(messages)
+        # print(f"cursor {cursor} ")
+        # print(messages)
         self.write(dict(messages=messages))
 
     def on_connection_close(self):
@@ -115,6 +122,7 @@ def make_app():
         (r"/", IndexHandler),
         (r"/websocket", EchoWebSocketHandler),
         (r"/message/update", MessageUpdatesHandler),
+        (r"/stat", StatHandler),
     ], debug = True)
 
 
